656027db314a5729ee1b425cba92c55c05200354
BestBlade <68151785+BestBlade@users.noreply.github.com>
2020-11-03 11:18:47 +0800

    Add files via upload

---
+++ leetcode_剑指offer25_合并两个排序的链表_w1.cpp
@@ -0,0 +1,67 @@
+/* ------------------------------------------------------------------|
+给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
+示例 1:
+输入: 1->2->3->4->5->NULL, k = 2
+输出: 4->5->1->2->3->NULL
+解释:
+向右旋转 1 步: 5->1->2->3->4->NULL
+向右旋转 2 步: 4->5->1->2->3->NULL
+示例 2:
+输入: 0->1->2->NULL, k = 4
+输出: 2->0->1->NULL
+解释:
+向右旋转 1 步: 2->0->1->NULL
+向右旋转 2 步: 1->2->0->NULL
+向右旋转 3 步: 0->1->2->NULL
+向右旋转 4 步: 2->0->1->NULL
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/rotate-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	
+*
+*	执行用时：40 ms, 在所有 C++ 提交中击败了89.55%的用户
+*	内存消耗：19.1 MB, 在所有 C++ 提交中击败了42.44%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    ListNode* node = new ListNode(0);
+    ListNode* preHead = node;
+    while (l1 && l2) {
+        if (l1->val <= l2->val) {
+            node->next = l1;
+            l1 = l1->next;
+        }
+        else {
+            node->next = l2;
+            l2 = l2->next;
+        }
+        node = node->next;
+    }
+    node->next = l1 ? l1 : l2;
+    return preHead->next;
+}
